4f561cb8fe63df0010f2a522ffbe7d26d0769ec2
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2025-07-14 15:40:05 -0300

    Update lista.h

--- Andre/lista.h
+++ Andre/lista.h
@@ -14,6 +14,6 @@ int     inserirFim    (Lista*,      int);
 int     removerMeio   (Lista*,      int);
 void    imprimirLista (Lista*          );
 int     getValor      (Celula*         );
-void    limparLista   (Lista* l        );
+void    limparLista   (Lista*          );
 
 #endif
